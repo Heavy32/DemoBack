af4dd8ff00958efaa2dd6ba4d90f2fab7d6c7a36
Максим Горанков <maksim.gorankov@rktv.ru>
2025-02-01 12:35:43 +0500

    Add project files.

---
+++ DemoBack/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DemoBack
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Record> Records { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ DemoBack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoBack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
